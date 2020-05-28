296ef3ebc7f0f231f07e30cc0d911a0c6e7542f1
Roman <rhombusural@gmail.com>
2020-05-29 00:00:24 +0500

    Moved DrywallRepo.java, Develop DrywallService.java

--- src/main/java/ru/gwp/team/app/_example/repos/DrywallRepo.java
+++
@@ -1,9 +0,0 @@
-package ru.gwp.team.app._example.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gwp.team.app.components.Drywall;
-
-import java.util.UUID;
-
-public interface DrywallRepo extends JpaRepository<Drywall, UUID> {
-}
