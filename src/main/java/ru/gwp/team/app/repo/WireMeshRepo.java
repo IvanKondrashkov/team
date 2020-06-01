0dd13301c854c3d2649a2bedf6464ac761273d2c
Alex-Chaikin <65180148+Alex-Chaikin@users.noreply.github.com>
2020-06-01 22:26:07 +0500

    Delete WireMeshRepo.java

--- src/main/java/ru/gwp/team/app/repo/WireMeshRepo.java
+++
@@ -1,9 +0,0 @@
-package ru.gwp.team.app.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gwp.team.app.components.WireMesh;
-
-import java.util.UUID;
-
-public interface WireMeshRepo extends JpaRepository<WireMesh, UUID> {
-}
